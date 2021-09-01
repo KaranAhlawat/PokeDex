63fd11445d87d37b5bae52ef94972a991ac06f1f
kyle g <kyleguerrero85@gmail.com>
2021-09-01 00:21:51 -0500

    chore: refactored project

--- PokeDex/Models/Pokeman.cs
+++ PokeDex/Models/Pokeman.cs
@@ -1,14 +1,32 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.Text.Json;
 
 namespace PokeDex.Models
 {
+    //public class Pokeman
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Url { get; set; }
+    //    public string Image { get; set; }
+
+    //    public override string ToString() => JsonSerializer.Serialize(this);
+    //}
+
+    public class APIResponse
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string? Previous { get; set; }
+        public List<Pokeman> Results { get; set; }
+    }
+
     public class Pokeman
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
         public string Image { get; set; }
-
-        public override string ToString() => JsonSerializer.Serialize(this);
     }
+
 }
